cc877068a854bfaf870f05dbbe3e39440ffed362
dominguezjavier <javier_rdominguez@yahoo.com>
2019-09-18 20:54:44 -0700

    Assign Random Icon Images

--- Matching_Pairs_Game/WindowsFormsApp1/Form1.cs
+++ Matching_Pairs_Game/WindowsFormsApp1/Form1.cs
@@ -5,7 +5,7 @@
  * project 1; create a matching pair game
  */
 
-System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,7 +15,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace matchingpairsgame
+namespace WindowsFormsApp1
 {
     public partial class matchingpairsgame : Form
     {
@@ -29,6 +29,22 @@ namespace matchingpairsgame
         public matchingpairsgame()
         {
             InitializeComponent();
+            AssignIconsToSquares();
+        }
+
+        private void AssignIconsToSquares()
+        {
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+                if (iconLabel != null)
+                {
+                    int randomnumber = random.Next(icons.Count);
+                    iconLabel.Text = icons[randomnumber];
+
+                    icons.RemoveAt(randomnumber);
+                }
+            }
         }
     }
 }
