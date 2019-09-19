a754d35bda1cf37f1db32a2512ffbcc525a8559b
dominguezjavier <javier_rdominguez@yahoo.com>
2019-09-18 20:30:21 -0700

    Creating Random Part 2

--- Matching_Pairs_Game/WindowsFormsApp1/Form1.cs
+++ Matching_Pairs_Game/WindowsFormsApp1/Form1.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*Javier Dominguez
+ * Reno Technology Academy
+ * CIS109; Coding in C# for imbedded systems
+ * Udemy 17 Beginner C#
+ * project 1; create a matching pair game
+ */
+
+System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,10 +15,17 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace WindowsFormsApp1
+namespace matchingpairsgame
 {
     public partial class matchingpairsgame : Form
     {
+        Random random = new Random();
+        List<string> icons = new List<string>()
+        {
+            "!","!","A","A","O","O","C","C",
+            "R","R","J","J","M","M","H","H",
+        };
+
         public matchingpairsgame()
         {
             InitializeComponent();
