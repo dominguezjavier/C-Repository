ecd1ca7d260a9e8b45c1626a835fbca41c1587c8
dominguezjavier <javier_rdominguez@yahoo.com>
2019-09-25 20:31:09 -0700

    Project 1 Finished

--- Matching_Pairs_Game/WindowsFormsApp1/Form1.cs
+++ Matching_Pairs_Game/WindowsFormsApp1/Form1.cs
@@ -17,34 +17,106 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
-    public partial class matchingpairsgame : Form
+    public partial class Form1 : Form
     {
         Random random = new Random();
+
         List<string> icons = new List<string>()
         {
             "!","!","A","A","O","O","C","C",
             "R","R","J","J","M","M","H","H",
         };
 
-        public matchingpairsgame()
+        Label firstclicked, secondclicked;
+
+        public Form1()
         {
             InitializeComponent();
             AssignIconsToSquares();
         }
 
+        private void label_Click(object sender, EventArgs e)
+        {
+            if (firstclicked != null && secondclicked != null)
+                return;
+
+            Label clickedLabel = sender as Label;
+
+            if (clickedLabel == null)
+                return;
+            if (clickedLabel.ForeColor == Color.Black)
+                return;
+            if (firstclicked == null)
+            {
+                firstclicked = clickedLabel;
+                firstclicked.ForeColor = Color.Black;
+                return;
+            }
+
+            secondclicked = clickedLabel;
+            secondclicked.ForeColor = Color.Black;
+
+            checkForwinner();
+
+            if (firstclicked.Text == secondclicked.Text)
+            {
+                firstclicked = null;
+                secondclicked = null;
+            }
+            else
+                timer1.Start();
+
+        }
+
+        private void checkForwinner()
+        {
+            Label label;
+            for(int i = 0; i < tableLayoutPanel1.Controls.Count; i++)
+            {
+                label = tableLayoutPanel1.Controls[i] as Label;
+
+                if (label != null && label.ForeColor == label.BackColor)
+                    return;
+            }
+
+            MessageBox.Show("You matched all the icons! Congrats!");
+            Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+
+            firstclicked.ForeColor = firstclicked.BackColor;
+            secondclicked.ForeColor = secondclicked.BackColor;
+
+            firstclicked = null;
+            secondclicked = null;
+
+        }
+
         private void AssignIconsToSquares()
         {
-            foreach (Control control in tableLayoutPanel1.Controls)
-            {
-                Label iconLabel = control as Label;
-                if (iconLabel != null)
-                {
-                    int randomnumber = random.Next(icons.Count);
-                    iconLabel.Text = icons[randomnumber];
+            Label label;
+            int randomNumber;
 
-                    iconLabel.ForeColor = iconLabel.BackColor;
-                    icons.RemoveAt(randomnumber);
+            for (int i = 0; i < tableLayoutPanel1.Controls.Count; i++)
+            {
+                if (tableLayoutPanel1.Controls[i] is Label)
+                {
+                    label = (Label)tableLayoutPanel1.Controls[i];
+                    randomNumber = random.Next(0, icons.Count);
+                    label.Text = icons[randomNumber];
+
+                    icons.RemoveAt(randomNumber);
                 }
+                else
+                    continue;
             }
         }
     }
--- Matching_Pairs_Game/WindowsFormsApp1/Program.cs
+++ Matching_Pairs_Game/WindowsFormsApp1/Program.cs
@@ -16,7 +16,7 @@ namespace WindowsFormsApp1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new matchingpairsgame());
+            Application.Run(new Form1());
         }
     }
 }
