8b8ae7e22b586c13a9d3490772370ba1dda633f7
dominguezjavier <javier_rdominguez@yahoo.com>
2019-10-16 20:27:51 -0700

    Commenting your code

--- pictureViewer/WindowsFormsApp1/Form1.cs
+++ pictureViewer/WindowsFormsApp1/Form1.cs
@@ -35,11 +35,14 @@ namespace WindowsFormsApp1
 
         private void clearButton_Click(object sender, EventArgs e)
         {
+            // clear the picture
             pictureBox1.Image = null;
         }
 
         private void showButton_Click(object sender, EventArgs e)
         {
+            //Show the open file dialog. IF the user clicks ok, load the 
+            //picture that the user chose
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 pictureBox1.Load(openFileDialog1.FileName);
@@ -48,6 +51,10 @@ namespace WindowsFormsApp1
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
+            //If the user selects the stretch check box,
+            //change the pictureBox's
+            // Sizecode property to "Stretch", If the user clears
+            // the check box, change it "Normal".
             if (checkBox1.Checked)
                 pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             else
