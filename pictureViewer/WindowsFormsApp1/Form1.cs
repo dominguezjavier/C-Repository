15c47a66a1d137791c0da9c3a73fba839792115d
dominguezjavier <javier_rdominguez@yahoo.com>
2019-10-16 20:15:13 -0700

    Write code for additional buttons and checkbox

--- pictureViewer/WindowsFormsApp1/Form1.cs
+++ pictureViewer/WindowsFormsApp1/Form1.cs
@@ -24,17 +24,18 @@ namespace WindowsFormsApp1
 
         private void closeButton_Click(object sender, EventArgs e)
         {
-
+            this.Close();
         }
 
         private void backgroundButton_Click(object sender, EventArgs e)
         {
-
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+                pictureBox1.BackColor = colorDialog1.Color;
         }
 
         private void clearButton_Click(object sender, EventArgs e)
         {
-
+            pictureBox1.Image = null;
         }
 
         private void showButton_Click(object sender, EventArgs e)
@@ -47,7 +48,10 @@ namespace WindowsFormsApp1
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
-
+            if (checkBox1.Checked)
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            else
+                pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
         }
     }
 }
