ffec8a9c2999a0517dd0f7c05c713a5b7aa54bf2
dominguezjavier <javier_rdominguez@yahoo.com>
2019-10-31 18:44:04 -0700

    Design The Calculator Part 1

--- Creating your own Calculator/Creating your own Calculator/Form1.Designer.cs
+++ Creating your own Calculator/Creating your own Calculator/Form1.Designer.cs
@@ -28,7 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.divideBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btn7
+            // 
+            this.btn7.BackColor = System.Drawing.Color.SkyBlue;
+            this.btn7.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn7.Location = new System.Drawing.Point(94, 59);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(75, 42);
+            this.btn7.TabIndex = 0;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = false;
+            // 
+            // btn9
+            // 
+            this.btn9.BackColor = System.Drawing.Color.SkyBlue;
+            this.btn9.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn9.Location = new System.Drawing.Point(288, 59);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(75, 42);
+            this.btn9.TabIndex = 1;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = false;
+            // 
+            // btn8
+            // 
+            this.btn8.BackColor = System.Drawing.Color.SkyBlue;
+            this.btn8.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn8.Location = new System.Drawing.Point(188, 59);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(75, 42);
+            this.btn8.TabIndex = 2;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = false;
+            // 
+            // divideBtn
+            // 
+            this.divideBtn.BackColor = System.Drawing.Color.SkyBlue;
+            this.divideBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.divideBtn.Location = new System.Drawing.Point(396, 59);
+            this.divideBtn.Name = "divideBtn";
+            this.divideBtn.Size = new System.Drawing.Size(75, 42);
+            this.divideBtn.TabIndex = 3;
+            this.divideBtn.Text = "÷";
+            this.divideBtn.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
@@ -36,16 +84,26 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(900, 450);
+            this.Controls.Add(this.divideBtn);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn7);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MinimizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button divideBtn;
     }
 }
 
--- Creating your own Calculator/Creating your own Calculator/Form1.cs
+++ Creating your own Calculator/Creating your own Calculator/Form1.cs
@@ -16,5 +16,10 @@ namespace Creating_your_own_Calculator
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
