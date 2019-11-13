27288f12aed4dc13cfe47689df074b0cd9c10d3a
dominguezjavier <javier_rdominguez@yahoo.com>
2019-11-13 14:57:20 -0800

    Adding A Sound Player

--- mazeGame/WindowsFormsApp1/Form1.cs
+++ mazeGame/WindowsFormsApp1/Form1.cs
@@ -11,7 +11,11 @@ using System.Windows.Forms;
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
+
     {
+        System.Media.SoundPlayer startSoundPlayer = new System.Media.SoundPlayer(@"C:\Windows\media\chord.wav");
+        System.Media.SoundPlayer finishSoundPlayer = new System.Media.SoundPlayer(@"C:\Windows\media\tada.wav");
+
         public Form1()
         {
             InitializeComponent();
@@ -20,12 +24,14 @@ namespace WindowsFormsApp1
 
         private void finishLabel_MouseEnter(object sender, EventArgs e)
         {
+            finishSoundPlayer.Play();
             MessageBox.Show("Congratulations!");
             Close();
         }
 
         private void MoveToStart()
         {
+            startSoundPlayer.Play();
             Point startingpoint = panel1.Location;
             startingpoint.Offset(10, 10);
             Cursor.Position = PointToScreen(startingpoint);
